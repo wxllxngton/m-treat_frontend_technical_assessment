502c0fdd76c110a86ae134430b47c90d9fa5ca5b
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2025-01-14 15:06:45 +0300

    update views.py: Login API View

--- backend/app/api/authentication/views.py
+++ backend/app/api/authentication/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
 from .serializers import UserSerializer, LoginSerializer
+from django.contrib.auth.hashers import check_password
 
 class UserRegistrationView(APIView):
     def post(self, request):
@@ -15,21 +16,29 @@ class UserRegistrationView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 class UserLoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             email = serializer.validated_data['email']
             password = serializer.validated_data['password']
-            user = authenticate(email=email, password=password)
-            if user:
+
+            try:
+                # Query the database for the user with the provided email
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+            # Check if the provided password matches the stored hashed password
+            if check_password(password, user.password):
                 refresh = RefreshToken.for_user(user)
                 return Response({
                     'refresh': str(refresh),
                     'access': str(refresh.access_token)
                 }, status=status.HTTP_200_OK)
-            return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserDetailView(APIView):
